df596a0e9b43219e76ecd309c41aa694e9bc8371
marcucakth <marcuca@kth.se>
2025-11-20 20:15:07 +0100

    Refactor accel_snittstorheter for new parameters
    Updated the function to include mass and acceleration parameters, and added calculations for air resistance and driving forces.

--- accel_snittstorheter.m
+++ accel_snittstorheter.m
@@ -1,16 +1,10 @@
-function [y_vec, Tyx, Tyz, N_kraft, Mx, My] = accel_snittstorheter(L, bb, b1, bd, N_points, Nb, Rix, Fa, Mk)
-% ACCEL_SNITTSTORHETER Beräknar snittkrafter för acceleration (6 intervall)
+function [y_vec, Tyx, Tyz, N_kraft, Mx, My] = accel_snittstorheter(L, bb, b1, bd, N_points, Nb, m, a_accel, Cd, A, rho, v, r_hjul, r_drev)
+% ACCEL_SNITTSTORHETER Beräknar snittkrafter för acceleration
 %
 % Inputs:
-%   L        - Total längd (m)
-%   bb       - Avstånd bb (m)
-%   b1       - Avstånd b1 (m)
-%   bd       - Avstånd bd (m)
-%   N_points - Antal punkter
-%   Nb       - Lagerkraft/Normalkraft (N)
-%   Rix      - Kraft Rix (N)
-%   Fk       - Kraft Fk (N)
-%   Mk       - Moment (Nm)
+%   m, a_accel    - Massa och acceleration
+%   Cd, A, rho, v - För luftmotstånd
+%   r_hjul, r_drev - Radier
 
     % --- Initiera vektorer ---
     y_vec = linspace(0, L, N_points);
@@ -21,57 +15,77 @@ function [y_vec, Tyx, Tyz, N_kraft, Mx, My] = accel_snittstorheter(L, bb, b1, bd
     Mx = zeros(1, N_points);      
     My = zeros(1, N_points);      
 
+    % --- 1. Beräkna krafter internt ---
+    
+    % Luftmotstånd
+    F_luft = 0.5 * rho * A * Cd * v^2;
+    
+    % Total drivkraft vid hjulet som krävs (Massa*a + Luftmotstånd)
+    Fd_accel = m * a_accel + F_luft;
+
+    % Drivmomentet på axeln
+    Mk = Fd_accel * r_hjul; 
+    
+    % Kedjekraften Fk
+    Fk = Mk / r_drev; 
+    
+    % Reaktionskraft Ryx (Högra lagret)
+    Ryx = (Fk * (b1 - bd)) / (2*b1 - L);
+    
+    % Reaktionskraft Rix (Vänstra lagret)
+    Rix = Ryx - Fk;
+
     % --- Loopa genom alla punkter ---
     for i = 1:N_points
-        y = y_vec(i); % Aktuell position
+        y = y_vec(i); 
         
-        % --- Intervall 1: 0 till bb ---
+        % Intervall 1: 0 till bb
         if y >= 0 && y < bb
-            Tyx(i)     = 0;          % Fyll i din formel
-            Tyz(i)     = 0;          % Fyll i din formel
-            N_kraft(i) = 0;          % Fyll i din formel
-            Mx(i)      = 0;          % Fyll i din formel
-            My(i)      = -Nb * y;          % Fyll i din formel
+            Tyx(i)     = 0;          
+            Tyz(i)     = -Nb;        
+            N_kraft(i) = 0;          
+            Mx(i)      = -Nb * y;    
+            My(i)      = 0;          
             
-        % --- Intervall 2: bb till b1 ---
+        % Intervall 2: bb till b1
         elseif y >= bb && y < b1
             Tyx(i)     = 0; 
             Tyz(i)     = -Nb; 
             N_kraft(i) = 0; 
-            Mx(i)      = 0; 
-            My(i)      = -Nb * y; 
+            Mx(i)      = -Nb * y; 
+            My(i)      = 0; 
 
-        % --- Intervall 3: b1 till bd ---
+        % Intervall 3: b1 till bd
         elseif y >= b1 && y < bd
-            Tyx(i)     = Rix; 
-            Tyz(i)     = 0; 
+            Tyx(i)     = Rix;        
+            Tyz(i)     = 0;          
             N_kraft(i) = 0; 
-            Mx(i)      = 0; 
-            My(i)      = -Nb * b1; 
+            Mx(i)      = -Nb * b1;   
+            My(i)      = 0; 
 
-        % --- Intervall 4: bd till L-b1 ---
+        % Intervall 4: bd till L-b1
         elseif y >= bd && y < (L - b1)
             Tyx(i)     = Rix - Fk; 
             Tyz(i)     = 0; 
             N_kraft(i) = 0; 
-            Mx(i)      = Mk; 
-            My(i)      = -Nb * b1; 
+            Mx(i)      = -Nb * b1; 
+            My(i)      = Mk;         
 
-        % --- Intervall 5: L-b1 till L-bb ---
+        % Intervall 5: L-b1 till L-bb
         elseif y >= (L - b1) && y < (L - bb)
             Tyx(i)     = 0; 
-            Tyz(i)     = Nb; 
+            Tyz(i)     = Nb;         
             N_kraft(i) = 0; 
-            Mx(i)      = Mk; 
-            My(i)      = -Nb * (y+L); 
+            Mx(i)      = Nb * (y - L); 
+            My(i)      = Mk;         
 
-        % --- Intervall 6: L-bb till L ---
+        % Intervall 6: L-bb till L
         elseif y >= (L - bb) && y <= L
             Tyx(i)     = 0; 
             Tyz(i)     = Nb; 
             N_kraft(i) = 0; 
-            Mx(i)      = Mk; 
-            My(i)      = -Nb * (y+L); 
+            Mx(i)      = Nb * (y - L); 
+            My(i)      = Mk; 
         end
     end
 end
