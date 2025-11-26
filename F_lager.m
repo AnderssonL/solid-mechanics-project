f9b4ab3a7bf7a6d38926464f193d2ecd4323be78
MarkSandb <markussandberg04@gmail.com>
2025-11-26 21:37:05 +0100

    Implementering lagerkrafter

--- F_lager.m
+++ F_lager.m
@@ -1,27 +1,47 @@
-function d = F_lager(H_bi, H_by, V_bi, V_by ,F_k,F_b)
+function d = F_lager(L, b_1, d_h, r_b, r_d, b_b, b_d, a1, a2, Cd, rho_luft, v, styrVariabel, H_bi, H_by, V_bi, V_by, F_d)
 syms R_yx R_ix R_yz R_iz R_iy
-% Alla dessa måste defineras
-F_d = 1;
-L = 10;
-B_1 = 0.1;
-d_h = 1;
-r_b = 1;
-r_a=1;
-r_d=1;
-b_b= 1;
-F_bi = F_b;
-F_by = F_b;
-b_1 = 4;
 
-b_d=1;
+% Beräkna F_d och F_b respektive
+
+F_luft=1/2*Cd*rho_luft*v^2;
+
+%Styrvariabel används för att välja lastfallen. Vi har ingen kraft på drevet när vi bromsar till exempel.
+
+if styrVariabel==1 %Acceleration konstant acceleration
+    F_k=((m*a1+F_luft)*d_h/2)/r_d;
+    F_b=0;
+
+end
+
+if styrVariabel==2 %Bromsning med konstant retardation
+    F_k=0;
+    F_b=((m*a2+F_luft)*d_h/2)/(r_b*2);
+
+end
+
+if styrVariabel==3 %Kurvtagning
+    F_k=(F_luft*d_h/2)/r_d;
+    F_b=0;
+
+end
+
+%Kraftekvationer
 
 ex = F_d+R_ix+R_yx+F_k-2*F_b;
+
 ey = H_bi+H_by-R_iy;
+
 ez = V_bi + V_by + R_iz+R_yz;
-Mx = L/2*(V_bi-V_by)+d_h/2*(H_bi+H_by)+ (L/2-B_1)*(R_iz+R_yz);
+
+%Momentekvationer
+Mx = L/2*(V_bi-V_by)+d_h/2*(H_bi+H_by)+ (L/2-b_1)*(R_iz+R_yz);
+
 My = -F_d * d_h/2-2*F_b*r_b +F_k*r_d;
-Mz = L/2*(F_d/2-F_d/2)+(L/2-b_b)*(F_bi-F_by)+(L/2-b_1)*(R_yx-R_ix)-(L/2-b_d)*F_k;
+
+Mz = L/2*(F_d/2-F_d/2)+(L/2-b_b)*(F_b-F_b)+(L/2-b_1)*(R_yx-R_ix)-(L/2-b_d)*F_k;
+
+%Definera
 eq1 = [ex ey ez Mx My Mz]  == [0 0 0 0 0 0];
 
-
+%Solve
 d = solve(eq1,[R_yx R_ix R_yz R_iz R_iy]);
