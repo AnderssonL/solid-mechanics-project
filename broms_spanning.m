a6f045d60d8ad5227a820288dd7c82b0a4d9801a
salmmanfred <32799244+salmmanfred@users.noreply.github.com>
2025-12-08 15:18:11 +0100

    tog bort delningen med 2

--- broms_spanning.m
+++ broms_spanning.m
@@ -10,32 +10,32 @@ function [normal, vrid, skjuv] = broms_spanning(d, D, N_points, b_b, b_1, b_d, L
         y = y_vec(i);
 
         if 0<=y && y<=b_b
-            normal(i) = d*(N_b*y)/(2*ii);
+            normal(i) = d*(N_b*y)/(ii);
             vrid(i)   = 0;
             skjuv(i)  = (4*N_b)/(3*a);
     
         elseif b_b<y && y<=b_1
-            normal(i) = d*(N_b*y)/(2*ii);
+            normal(i) = d*(N_b*y)/(ii);
             vrid(i)   = d*(M_b1)/(2*k);
             skjuv(i)  = (4*sqrt(F_b^2 + N_b^2))/(3*a);
     
         elseif b_1<y && y<=b_d
-            normal(i) = D*(2*N_b*y+N_b*b_1)/(2*I);
+            normal(i) = D*(2*N_b*y+N_b*b_1)/(I);
             vrid(i)   = D*(M_b1)/(2*K);
             skjuv(i)  = 0;
     
         elseif b_d<y && y<=L-b_1
-            normal(i) = D*(2*N_b*y+N_b*b_1)/(2*I);
+            normal(i) = D*(2*N_b*y+N_b*b_1)/(I);
             vrid(i)   = D*(M_b1)/(2*K);
             skjuv(i)  = 0;
     
         elseif L-b_1<y && y<=L-b_b
-            normal(i) = d*(N_b*L+N_b*y)/(2*ii);
+            normal(i) = d*(N_b*L+N_b*y)/(ii);
             vrid(i)   = d*(M_b1)/(2*k);
             skjuv(i)  = (4*sqrt(F_b^2 + N_b^2))/(3*a);
     
         elseif L-b_b<y && y<=L
-            normal(i) = d*(N_b*L+N_b*y)/(2*ii);
+            normal(i) = d*(N_b*L+N_b*y)/(ii);
             vrid(i)   = 0;
             skjuv(i)  = (4*N_b)/(3*a);
     
