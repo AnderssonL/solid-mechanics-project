204976daed1bcd9bfeaf3701d9862a1626cdb8dc
Marcus <Marcus@LAPTOP-MARCUS>
2025-12-08 16:23:55 +0100

    fixat i y = L

--- anpassad_snittstorheter.m
+++ anpassad_snittstorheter.m
@@ -130,7 +130,7 @@ end
 
 if y == L
     Tyz_int = -Vbi - Riz - Ryz - Vby;    
-    Tyx_int = -F_driv/2 + 2*F_broms - Rix - Fk - Ryx - F_driv/2;
+    Tyx_int = -F_driv + 2*F_broms - Rix - Fk - Ryx;
     N = Hbi - Riy + Hby;
     Mx_int = Tyz_int * y - Mbi + Riz * b1 + Ryz * (L-b1) - Mby + Vby * L;
     My_int = 2 * M_D - 2 * Mb + Mk;
