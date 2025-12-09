51d38b812811218427aed97678c1b7664528e617
marcucakth <marcuca@kth.se>
2025-12-09 15:15:22 +0100

    satte in korrekta inputs i kurvtagning_hjulkrafter, satte gamma till korrekt värde

--- main.m
+++ main.m
@@ -18,18 +18,6 @@ kalradie = 4e-3;   % Kälradie [m]
 g = 9.82;           % Tyngdacceleration [m/s^2]
 rho_luft = 1.225;   % Luftens densitet [kg/m^3]
 
-a1 = 6;             % Acceleration i accelerationslastfallet [m/s^2]
-a2 = 15;           % Max retardation (värdet är positivt) [m/s^2]
-v_max_ms = 72/3.6;    % Maxfart (rakt fram) [km/h] (70-120)
-R_kurva = 15;       % Kurvradie [m] (5-15 m)
-gamma = sqrt(R_kurva*g*L/(2*hTP*v_max_ms^2));        % Faktor för max hastighet i kurva          Bestäm!
-v_kurva_ms = gamma * v_max_ms; % Hastighet i kurvan [m/s]
-v_accel = 1;        % Sätt en låg hastighet [m/s]
-
-n_s = 2.5;          % Säkerhetsfaktor mot plastisk deformation 
-n_u = 2.0;          % Säkerhetsfaktor mot utmattning
-
-
 %% Rear axel parameters
 L = 1.2; % axel längd
 D = 0.1; % axel diameter (tjockare del) Bestäm!
@@ -53,10 +41,23 @@ h = 0.001; % tidssteg för iteration
 N = L/h; % antalg iterationer
 y_vector = linspace(0, L, N);
 
+%% Acceleration and force parameters
+a1 = 6;             % Acceleration i accelerationslastfallet [m/s^2]
+a2 = 15;           % Max retardation (värdet är positivt) [m/s^2]
+v_max_ms = 72/3.6;    % Maxfart (rakt fram) [km/h] (70-120)
+R_kurva = 15;       % Kurvradie [m] (5-15 m)
+gamma = (1/v_max_ms)*sqrt((R_kurva*g*L)/(2*hTP));        % Faktor för max hastighet i kurva          Bestäm!
+v_kurva_ms = gamma * v_max_ms; % Hastighet i kurvan [m/s]
+v_accel = 1;        % Sätt en låg hastighet [m/s]
+
+n_s = 2.5;          % Säkerhetsfaktor mot plastisk deformation 
+n_u = 2.0;          % Säkerhetsfaktor mot utmattning
+
+
 %% Hjulkrafter calc
 [FD_accel, Nf_accel, Nb_accel, Mb_accel] = accel_hjulkrafter(df, db, h_luft, hTP, m, Cd, rho_luft, v_accel, a1, r_hjul, g);
 [FB_broms, FL_broms, Nf_broms, Nb_broms, Mb_broms] = broms_hjulkrafter(df, db, h_luft, h, m, Cd, rho_luft, v_max_ms, a2, r_hjul, g);
-[V_bi_kurva, V_fi_kurva, V_by_kurva, V_fy_kurva, H_bi_kurva, H_fi_kurva, H_by_kurva, H_fy_kurva, Mb_kurva, FD_kurva, V_i_kurva, V_y_kurva, H_i_kurva, H_y_kurva] = kurvtagning_hjulkrafter(df, db, h_luft, h, m, Cd, rho_luft, v_kurva_ms, R_kurva, L, r_hjul, g);
+[V_bi_kurva, V_fi_kurva, V_by_kurva, V_fy_kurva, H_bi_kurva, H_fi_kurva, H_by_kurva, H_fy_kurva, Mb_kurva, FD_kurva, V_i_kurva, V_y_kurva, H_i_kurva, H_y_kurva] = kurvtagning_hjulkrafter(df, db, hFL, hTP, m, Cd, rho_luft, v_kurva_ms, R_kurva, L, r_hjul, g);
 
 %% Lagerkrafter calc
 [R_yx_broms, R_ix_broms, R_yz_broms, R_iz_broms, R_iy_broms, Fk_broms, F_broms_broms]=F_lager(m, L, b_1, r_hjul, r_broms, r_drev, b_b, b_d, a1, a2, Cd, rho_luft, v_max_ms, 2, 0, 0, Nb_broms/2, Nb_broms/2, -FB_broms); %Skicka in bromsinput
